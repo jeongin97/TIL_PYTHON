39264ec90584c0e11e1ddeb600a16882d3912e12
jeongin97 <86945091+jeongin97@users.noreply.github.com>
2022-01-16 20:09:49 +0900

    Create DAY2_test.py

---
+++ Day_by_Day/DAY2_test.py
@@ -0,0 +1,7 @@
+num10=10                    # 10진수
+num16=0xa                   # 16진수
+num2= 0b1010                # 2진수
+
+print(num10, num16, num2)
+print("-"*30)
+print(num10, num16, num2, sep="\n")
