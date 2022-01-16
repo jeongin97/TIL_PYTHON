a12c6d3cb4d5508b5a0d11c5ecf61632f9115511
jeongin97 <86945091+jeongin97@users.noreply.github.com>
2022-01-16 20:02:24 +0900

    Update conditionLab1.py

--- 실습/conditionLab1.py
+++ 실습/conditionLab1.py
@@ -9,3 +9,12 @@ num = int(input_data)
 if num > 10 :
     print("OK")
 ''''''
+
+# [실습 2]
+color_name = input("color_name 을 입력하세요 :")
+if color_name == "red" :
+    print("'#ffOOOO'")
+if color_name != "red" :
+    print("'#OOOOOO'")
+
+    
