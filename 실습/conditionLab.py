82b045fad92573bda4a90a0b1c98d62d94e99fda
jeongin97 <86945091+jeongin97@users.noreply.github.com>
2022-01-16 20:04:45 +0900

    Update conditionLab.py

--- 실습/conditionLab.py
+++ 실습/conditionLab.py
@@ -2,7 +2,6 @@
 num = int(input("숫자를 입력하세요 : "))
 if num > 10 :
     print("OK")
-
 ''''''
 input_data = input("숫자를 입력하세요 : ")
 num = int(input_data)
@@ -10,12 +9,14 @@ if num > 10 :
     print("OK")
 ''''''
 
+
 # [실습 2]
 color_name = input("color_name 을 입력하세요 :")
 if color_name == "red" :
     print("'#ffOOOO'")
 if color_name != "red" :
     print("'#OOOOOO'")
+    
     
 # [실습 3]
 import random
@@ -25,6 +26,15 @@ if grade >= 1 and grade < 4 :
 else :
     print (grade, "학년은 고학년입니다.")
 
-   
+    
+# [실습 4]
+import random
+grade = random.randint(1, 6)
+print("grade(", grade, ") : ", sep="")
+
+if grade == 1 or grade == 2 or grade == 3 :
+    print (grade, "학년은 저학년입니다.")
+else :
+    print (grade, "학년은 고학년입니다.")   
     
     
