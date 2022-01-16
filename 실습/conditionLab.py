951fee629933dd9bae1547d984b5571209810cfc
jeongin97 <86945091+jeongin97@users.noreply.github.com>
2022-01-16 20:03:20 +0900

    Update and rename conditionLab1.py to conditionLab.py

--- 실습/conditionLab.py
+++ 실습/conditionLab.py
@@ -16,5 +16,16 @@ if color_name == "red" :
     print("'#ffOOOO'")
 if color_name != "red" :
     print("'#OOOOOO'")
+    
+# [실습 3]
+import random
+grade = random.randint(1, 6)
 
+if grade >= 1 and grade < 4 :
+    print (grade, "학년은 저학년입니다.")
+else :
+    print (grade, "학년은 고학년입니다.")
+
+   
+    
     
