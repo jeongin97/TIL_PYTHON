a316510194101a6616f779695050cffe5f74bd3a
jeongin97 <86945091+jeongin97@users.noreply.github.com>
2022-01-16 20:04:02 +0900

    Delete conditionLab4.py

--- 실습/conditionLab4.py
+++
@@ -1,9 +0,0 @@
-# [실습 4]
-import random
-grade = random.randint(1, 6)
-
-print("grade(", grade, ") : ", sep="")
-if grade == 1 or grade == 2 or grade == 3 :
-    print (grade, "학년은 저학년입니다.")
-else :
-    print (grade, "학년은 고학년입니다.")
