f7dc5323069b0598967934a9871123abc2a5d9b8
jeongin97 <86945091+jeongin97@users.noreply.github.com>
2022-01-16 20:13:49 +0900

    Create 1_basic.py

---
+++ 실습/1_basic.py
@@ -0,0 +1,41 @@
+# [실습 1]
+v1 = 100
+v2 = 50
+v1 ; v2
+
+r1 = v1 + v2
+r2 = v1 - v2
+r3 = v1 * v2
+r4 = v1 / v2
+print('r1 = ', r1)
+print('r2 = ', r2)
+print('r3 = ', r3)
+print('r4 = ', r4)
+
+
+# [실습 2]
+name1 = "윤"
+name2 = "정"
+name3 = "인"
+print(name1, name2, name3, sep='')
+print(name1, name2, name3, sep='@')
+print(name1, name2, name3, sep='---')
+
+
+# [실습 3]
+a1 = 10 ; a2 = 25 ; a3 = 34
+s = (a1 + a2 + a3)
+m = s/3
+print("합은 ", s, ' 이고 평균은 ', m, ' 입니다.')
+
+
+# [실습 4]
+num1 = int(input("숫자1 입력 : "))
+num2 = int(input("숫자2 입력 :"))
+print(num1,"+", num2,'=', num1 + num2)
+print(num1, "*", num2, "=", num1*num2)
+
+num1 = int(input("숫자1 입력 :"))
+num2 = int(input("숫자2 입력 :"))
+print(num1,"+", num2,'=', num1 + num2)
+print(num1, "*", num2, "=", num1*num2)
