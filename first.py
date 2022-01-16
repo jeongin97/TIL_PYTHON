a591cf22b42d8bf5f809444258b22d0511d3b63d
jeongin97 <86945091+jeongin97@users.noreply.github.com>
2022-01-16 17:00:28 +0900

    Add files via upload

---
+++ first.py
@@ -0,0 +1,12 @@
+for y in range(1, 20):
+    for x in range(y):
+        print('*', end='')
+    print()
+
+
+print("시작")
+for y in range(1, 10):
+    for x in range(y):
+        print('*', end='')
+    print()
+print('종료')
