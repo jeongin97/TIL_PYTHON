1014e727091c935bf6ddc3a6154e291b8bf400b0
jeongin97 <86945091+jeongin97@users.noreply.github.com>
2022-01-18 18:57:31 +0900

    Update DAY5_third.py

--- Day_by_Day/DAY5_third.py
+++ Day_by_Day/DAY5_third.py
@@ -49,3 +49,30 @@ printsum(4)
 printsum(10)
 
 #0709
+# 실습 1
+h = int(input("height : "))
+for i in range(1, h+1):
+    for j in range(i) :
+        print('*', end='')
+    print()
+def Counter (n) :
+    sum = 0
+    for num in range(n+1) :
+        sum += num
+    return sum
+print ("star 개수 : ", Counter(h))
+
+# 실습 1 다른 풀이
+def star(x) :
+    i = 0
+    for stage in range(1, x+1) :
+        if stage <=x :
+            print('*' * stage)
+            i += stage
+    return i
+star_count = star(3)
+print("star 개수 : ", star_count)
+
+# 연습
+price = 500
+print("궁금하면 " + str(price) + "원!")
